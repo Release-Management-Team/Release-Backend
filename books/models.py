629e461c15f2f161e72bab885e20f94a5cd8d585
catenary91 <sweetonion135@gmail.com>
2024-07-18 16:59:52 +0900

    Add notice, activity and edit models

--- books/models.py
+++ books/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 
 class Book(models.Model):
-    id = models.CharField(primary_key=True, max_length=100)
-    name = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    publisher = models.CharField(max_length=100)
-    available = models.BooleanField()
-    tags = models.ManyToManyField("BookTag")
-
-class BookRecord(models.Model):
-    borrower = models.IntegerField() # student id (foreign key)
-    book = models.ForeignKey(Book, models.PROTECT)
-    start_date = models.DateField()
-    end_date = models.DateField(null=True)
-
-class BookTag(models.Model):
-    tag = models.CharField(max_length=100)
+    id          = models.CharField(primary_key=True, max_length=100)
+    name        = models.CharField(max_length=100)
+    author      = models.CharField(max_length=100)
+    available   = models.BooleanField()
+    tags        = models.ManyToManyField('BookTag', related_name='books', blank=True)
+    donor       = models.ForeignKey('members.Member', models.PROTECT, blank=True, null=True)
+    image       = models.CharField(max_length=10, null=True)
 
     def __str__(self):
-        return self.tag
+        return f'{self.name}'
+
+class BookRecord(models.Model):
+    borrower        = models.ForeignKey('members.Member', models.PROTECT)
+    book            = models.ForeignKey(Book, models.PROTECT)
+    start           = models.DateField()
+    actual_return   = models.DateField(null=True)
+    
+
+class BookTag(models.Model):
+    tag = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f'#{self.tag}'
+    
--- members/models.py
+++ members/models.py
@@ -1,32 +1,32 @@
 from django.db import models
 
 class State:
-    ENROLLED = 0
-    ABSENCE = 1
+    ENROLLED  = 0
+    ABSENCE   = 1
     GRADUATED = 2
-    choices = (
-        (ENROLLED, '재학'),
-        (ABSENCE, '휴학'),
+    choices   = (
+        (ENROLLED,  '재학'),
+        (ABSENCE,   '휴학'),
         (GRADUATED, '졸업'),
     )
 
 class Role:
-    MEMBER = 0
-    STAFF = 1
-    PRESIDENT = 2
+    MEMBER  = 0
+    STAFF   = 1
     choices = (
         (MEMBER, '학회원'),
-        (STAFF, '임원'),
-        (PRESIDENT, '학회장'),
+        (STAFF,  '임원'),
     )
     
 
 class Member(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=10)
-    password = models.BinaryField(max_length=100)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField()
-    state = models.IntegerField(choices=State.choices)
-    role = models.IntegerField(choices=Role.choices)
-    message = models.CharField(max_length=300)
+    id          = models.IntegerField(primary_key=True)
+    name        = models.CharField(max_length=10)
+    password    = models.BinaryField(max_length=100) # byte array encrypted by bcrpyt
+    fcm_token   = models.CharField(null=True)
+    phone       = models.CharField(max_length=20)
+    email       = models.EmailField()
+    state       = models.IntegerField(choices=State.choices)
+    role        = models.IntegerField(choices=Role.choices)
+    message     = models.CharField(max_length=300)
+    image       = models.CharField(max_length=10)
