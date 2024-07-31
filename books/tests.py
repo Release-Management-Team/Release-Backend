861e03b137e2d57e5d5c553f4a7e6870fd50cf3a
catenary91 <sweetonion135@gmail.com>
2024-07-31 18:44:03 +0900

    books: add decorators to views

--- books/tests.py
+++ books/tests.py
@@ -8,7 +8,7 @@ class BookTestCase(TestCase):
         BookTag.objects.create(tag='System')
         BookTag.objects.create(tag='OS')
         
-        Book.objects.create(id='1', name='test book 1', author='신현수', available=True, image='').tags.add(BookTag.objects.filter(tag='System'))
+        Book.objects.create(id='1', name='test book 1', author='신현수', available=True, image='')
         Book.objects.create(id='2', name='test book 2', author='정재헌', available=False, image='')
         
     def test_book_list(self):
