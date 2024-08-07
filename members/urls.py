b8839d5447da30eda8f20c5f4901d1f6b152ed9d
SeoLanee <seolan25@gmail.com>
2024-08-07 16:47:59 +0900

    feature: members urls update

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-app_name = 'common'
+from . import views
 
 urlpatterns = [
-    path('login/')
+    path('my-profile', views.get_my_profile),
+    path('update-profile', views.update_my_profile),
+    path('change-password', views.change_password),
+    path('member-list', views.get_members_list),
+    path('member-profile', views.get_member_profile),
 ]
