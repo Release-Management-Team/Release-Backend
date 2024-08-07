9d936fc45dc27b32616620ed1c86a1d560dc4e50
catenary91 <sweetonion135@gmail.com>
2024-08-07 17:41:19 +0900

    activities: delete abstract model

--- activities/models.py
+++ activities/models.py
@@ -1,15 +1,31 @@
 from django.db import models
 
-class Activity(models.Model):
-    name     = models.CharField(max_length=100)
-    jjang    = models.ForeignKey('members.Member', models.PROTECT, related_name='%(class)s_jjang')
-    members  = models.ManyToManyField('members.Member', related_name='%(class)s', blank=True)
+class Study(models.Model):
+    name = models.CharField(max_length=100)
+    president = models.ForeignKey('members.Member', models.PROTECT, related_name='running_studies')
+    members = models.ManyToManyField('members.Member', related_name='studies')
+    goal = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
 
-    class Meta:
-        abstract = True
+    def to_dict(self, fields):
+        data = {}
+        for name in fields:
+            if name == 'president':
+                data[name] = self.president.id
+            elif name == 'members':
+                data[name] = [member.id for member in self.members.all()]
+            else:
+                data[name] = self._meta.get_field(name).value_from_object(self)
 
-class Study(Activity):
-    pass
+        return data
 
-class Project(Activity):
-    pass
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    president = models.ForeignKey('members.Member', models.PROTECT, related_name='running_projects')
+    members = models.ManyToManyField('members.Member', related_name='projects')
+    description = models.CharField(max_length=1000)
+
+    def to_dict(self, fields):
+        return { name: self._meta.get_field(name).value_from_object(self) for name in fields }
+    
--- activities/views.py
+++ activities/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from django.views.decorators.http import require_GET
 
-# Create your views here.
+from .models import Study, Project
+
+from jwt_auth.decorator import check_access_token
+
+@require_GET
+@check_access_token
+def list_studies(request:HttpRequest):
+    data = {'studies': [study.to_dict(['name', 'president', 'members', 'goal', 'description']) for study in Study.objects.all()] }
+    return JsonResponse(data)
+
