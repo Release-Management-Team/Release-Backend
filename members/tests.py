0d7191f86101f06ec40060caa0243f0ae6373d73
SeoLanee <seolan25@gmail.com>
2024-08-07 19:38:20 +0900

    feature: Implement update-profile, change-password

--- members/tests.py
+++ members/tests.py
@@ -1,4 +1,4 @@
-import json
+import json, bcrypt
 
 from django.test import TestCase
 
@@ -26,11 +26,12 @@ class MemberTestCase(TestCase):
 
     def test_get_my_profile(self):
         print('\nTesting get_my_profile...')
+
         response = self.client.get('/members/my-profile', headers=self.headers)
         self.assertEqual(response.status_code, 200)
 
         json_response = response.json()
-        self.assertEqual(20201641, json_response.get('id'))
+        self.assertEqual('20201641', json_response.get('id'))
         self.assertEqual('정재헌', json_response.get('name'))
         self.assertEqual('01000000000', json_response.get('phone'))
         self.assertEqual('test0@gmail.com', json_response.get('email'))
@@ -38,23 +39,47 @@ class MemberTestCase(TestCase):
 
     def test_update_my_profile(self):
         print('\nTesting update_my_profile...')
-        pass
+        data = {
+            'phone': '01012345678',
+            'email': 'deadbeaf@gmail.com',
+            'message': 'you cracked',
+            'image': 'imagine'
+        }
+        response = self.client.post('/members/update-profile', headers=self.headers, data=json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+        member = Member.objects.get(id='20201641')
+        self.assertEqual(member.phone, '01012345678')
+        self.assertEqual(member.email, 'deadbeaf@gmail.com')
+        self.assertEqual(member.message, 'you cracked')
+        self.assertEqual(member.image, 'imagine')
 
 
     def test_change_password(self):
         print('\nTesting change_password...')
-        pass
+
+        new_password = 'new_password'
+        data = {
+            'password': new_password
+        }
+        response = self.client.post('/members/change-password', headers=self.headers, data=data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+        member = Member.objects.get(id='20201641')
+        self.assertTrue(bcrypt.checkpw(new_password.encode('utf-8'), member.password.tobytes()))
+
 
 
     def test_get_members_list(self):
         print('\nTesting get_members_list...')
+
         response = self.client.get('/members/member-list', headers=self.headers)
         json_response = response.json()
         profiles = json_response.get('profiles')
         for i in profiles:
             print(i)
 
-        
+
     def test_get_member_profile(self):
         print('\nTesting get_member_profile...')
         pass
--- members/views.py
+++ members/views.py
@@ -1,8 +1,9 @@
-from django.http import JsonResponse
+import bcrypt
+
+from django.http import HttpResponse, JsonResponse
 
 from jwt_auth.decorators import *
 from utils.decorators import *
-# Create your views here.
 
 @check_access_token
 @use_member
@@ -24,16 +25,40 @@ def get_my_profile(request, member):
 @use_body
 @use_member
 def update_my_profile(request, body, member):
-    pass
+    try:
+        if 'phone' in body:
+            member.phone = body.get('phone')
+        
+        if 'email' in body:
+            member.email = body.get('email')
+        
+        if 'message' in body:
+            member.message = body.get('message')
 
+        if 'image' in body:
+            member.image = body.get('image')
+    
+        member.save()
+    except KeyError:
+        return HttpResponseBadRequest()
+    finally:
+        return HttpResponse(200)
 
 
 @check_access_token
 @use_body
 @use_member
-def change_password(request):
-    pass
+def change_password(request, body, member):
+    try:
+        pw = body.get('password')
+        encoded = bcrypt.hashpw(pw.encode('utf-8'), bcrypt.gensalt())
+        member.password = encoded
 
+        member.save()
+    except:
+        return HttpResponseBadRequest()
+    finally:
+        return HttpResponse(200)
 
 
 @check_access_token
@@ -48,5 +73,5 @@ def get_members_list(request):
 
 @check_access_token
 @use_body
-def get_member_profile(request):
+def get_member_profile(request, body):
     pass
