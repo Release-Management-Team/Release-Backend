2c4613120a0504f382e90652ce57bb4607c22a35
catenary91 <sweetonion135@gmail.com>
2024-09-01 15:59:13 +0900

    utils: change use_body to get parameters

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,14 +1,40 @@
 import json
 from functools import wraps
 
-from django.http import HttpResponseBadRequest
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
-def use_body(func):
-    @wraps(func)
-    def wrapper(request, *args, **kwargs):
-        if not request.body:
-            return HttpResponseBadRequest()
-        body = json.loads(request.body)
-        return func(request, *args, **kwargs, body=body)
+def use_body(*fields: str):
     
-    return wrapper
+    def decorator(func):
+        @wraps(func)
+        def wrapper(request, **kwargs):
+            if not request.body:
+                return JsonResponse({'error': 'ERR_MISSING_FIELD'})
+            body = json.loads(request.body)
+
+            for name in fields:
+                if name not in body:
+                    return JsonResponse({'error': 'ERR_MISSING_FIELD'})
+            
+            return func(request, **kwargs, body=body)
+        
+        return wrapper
+    return decorator
+
+def use_params(*fields: str):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(request, **kwargs):
+            if not request.GET:
+                return JsonResponse({'error': 'ERR_MISSING_FIELD'})
+            for name in fields:
+                if name not in request.GET:
+                    return JsonResponse({'error': 'ERR_MISSING_FIELD'})
+            
+            params = request.GET
+            return func(request, **kwargs, params=params)
+        
+        return wrapper
+    return decorator
