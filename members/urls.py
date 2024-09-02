06059f5042c48d1ebcb5d75d8abd080b4ce26a1d
catenary91 <sweetonion135@gmail.com>
2024-09-02 16:55:05 +0900

    members: add model fields and change urls

--- members/urls.py
+++ members/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('my-profile', views.get_my_profile),
-    path('update-profile', views.update_my_profile),
+    path('profile', views.get_my_profile),
+    path('profile/update', views.update_my_profile),
     path('change-password', views.change_password),
     path('member-list', views.get_members_list),
     path('member-profile', views.get_member_profile),
