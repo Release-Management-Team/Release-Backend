d4625e2c68eaba1835ea95559b2f8d333953d6c0
SeoLanee <seolan25@gmail.com>
2025-01-05 19:28:48 +0900

    Members-Change urls

--- members/urls.py
+++ members/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('/', views.get_members_list),
+    path('/<int:student_id>', views.get_member_profile),
+    path('/my-profile', views.get_my_profile),
+    path('/change-password', views.change_password),
+
     path('home', views.home),
-    path('profile', views.get_my_profile),
-    path('profile/update', views.update_my_profile),
-    path('change-password', views.change_password),
-    path('member-list', views.get_members_list),
-    path('member-profile', views.get_member_profile),
 ]
