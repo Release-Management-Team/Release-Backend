23602cee4abdaa4b7732e3c283c70d03f113bad7
SeoLanee <seolan25@gmail.com>
2025-01-30 22:09:10 +0900

    FCM: add urls

--- members/urls.py
+++ members/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.members_list),
     path('<int:student_id>', views.member_profile),
+    path('register-device', views.register_device),
     path('my-profile', views.my_profile),
     path('my-profile/update', views.update_my_profile),
     path('my-profile/change-password', views.change_password),
